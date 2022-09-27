5fb0eb10412b7d50cef5d28d9201387874ac729f
mohamedubeid <abomuin112@hotmail.com>
2022-09-27 10:47:15 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import HomePage from './pages/HomePage';
+// import Profile from './pages/Profile';
+// import { Divider } from '@mui/material';
+// import authContext from './auth-context';
+
+// function App() {
+//     const [isLoggedIn, setLoggedIn] = useState(false);
+//     const logIn = () => {
+//         setLoggedIn(!isLoggedIn);
+//     };
+
+//     return (
+//         <BrowserRouter>
+//             <authContext.Provider value={{ status: isLoggedIn, login: logIn }}>
+//                 <Navbar />
+//                 <Divider
+//                     sx={{
+//                         width: '100%',
+//                         position: { xs: 'static', md: 'fixed' },
+//                     }}
+//                 />
+//                 <Routes>
+//                     <Route path="/home" element={<HomePage />} />
+//                     <Route path="/profile" element={<Profile />} />
+//                 </Routes>
+//             </authContext.Provider>
+//         </BrowserRouter>
+//     );
+// }
+
+// export default App;
+import { useState } from 'react';
 import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 import Profile from './pages/Profile';
 import { Divider } from '@mui/material';
 import authContext from './auth-context';
@@ -13,21 +46,16 @@ function App() {
     };
 
     return (
-        <BrowserRouter>
-            <authContext.Provider value={{ status: isLoggedIn, login: logIn }}>
-                <Navbar />
-                <Divider
-                    sx={{
-                        width: '100%',
-                        position: { xs: 'static', md: 'fixed' },
-                    }}
-                />
-                <Routes>
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/profile" element={<Profile />} />
-                </Routes>
-            </authContext.Provider>
-        </BrowserRouter>
+        <authContext.Provider value={{ status: isLoggedIn, login: logIn }}>
+            <Navbar />
+            <Divider
+                sx={{
+                    width: '100%',
+                    position: { xs: 'static', md: 'fixed' },
+                }}
+            />
+            <Profile />
+        </authContext.Provider>
     );
 }
 
